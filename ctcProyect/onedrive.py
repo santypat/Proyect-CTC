f14f7e965b3f4e93616e96bd7798b17708dca048
Zyk0d3 <damian.pphh@gmail.com>
2023-05-18 11:47:18 -0500

    lectura de carpeta

--- ctcProyect/onedrive.py
+++ ctcProyect/onedrive.py
@@ -1,3 +1,12 @@
-from pyonedrive import OneDrive
+import pyonedrive
 import os
-contenido = os.listdir('C:\Users\damian.pulgarin\OneDrive - arus.com.co\ASIGNACION PBS')
+
+carpeta ='C:\\Users\\damian.pulgarin\\OneDrive - arus.com.co\\ASIGNACION PBS'
+archivos = os.listdir(carpeta)
+
+for archivo in archivos:
+    ruta_archivo = os.path.join(carpeta, archivo)
+    with open(ruta_archivo, 'r') as f:
+        contenido = f.read()
+        
+        print(contenido)
